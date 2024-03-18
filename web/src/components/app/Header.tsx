0bb159d221e3a897b137525514be54ced989f9be
Rudney Rodrigues <rudney.un2016@gmail.com>
2024-03-17 22:35:48 -0300

    :iphone: Adicionando o Aside Sheet para responsividade

--- web/src/components/app/Header.tsx
+++ web/src/components/app/Header.tsx
@@ -1,20 +1,19 @@
 import Link from 'next/link'
 import { Notepad } from '@phosphor-icons/react'
-import { UpdateIcon } from '@radix-ui/react-icons'
 
-import { Button } from '../ui/button'
 import { ModeToggle } from './ModeToggle'
 import { NewNote } from './Dialog/NewNote'
+import { AsideSheet } from './Dialog/Aside'
 import { UserDropdown } from './Dropdown/User'
-import { useGetNotes } from '@/lib/swr/useGetNotes'
-import { Tooltip, TooltipTrigger, TooltipContent } from '../ui/tooltip'
 
 const Header = () => {
-  const { mutate, isLoading } = useGetNotes()
-
   return (
     <header className="h-16 sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="h-16 w-full max-w-screen-xl mx-auto px-4 flex items-center gap-2">
+        <div className="flex lg:hidden">
+          <AsideSheet />
+        </div>
+
         <div className="flex items-center gap-2">
           <Notepad
             size={32}
@@ -27,20 +26,6 @@ const Header = () => {
         </div>
 
         <div className="ml-auto flex items-center gap-6">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                size="icon"
-                variant="ghost"
-                isLoading={isLoading}
-                onClick={() => mutate()}
-              >
-                <UpdateIcon />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent>Atualizar</TooltipContent>
-          </Tooltip>
-
           <div className="flex items-center gap-2">
             <NewNote />
             <ModeToggle />
