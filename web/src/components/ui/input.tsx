f31b82466cae5bc5fd60eca262fda9d24d9651e7
Rudney Rodrigues <rudney.un2016@gmail.com>
2024-03-17 22:36:43 -0300

    ::sparkles: feat: Novos componentes

--- web/src/components/ui/input.tsx
+++ web/src/components/ui/input.tsx
@@ -9,10 +9,14 @@ export interface InputProps
   name: string
   label?: string
   error?: FieldError
+  height?: 'sm' | 'md' | 'lg'
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, name, label, error = null, ...props }, ref) => {
+  (
+    { className, type, name, label, height = 'md', error = null, ...props },
+    ref,
+  ) => {
     return (
       <div className="flex flex-col w-full gap-2">
         {!!label && <Label htmlFor={name}>{label}</Label>}
@@ -21,8 +25,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           id={name}
           name={name}
           type={type}
+          data-size={height}
           className={cn(
-            'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus:outline-2 focus:outline-offset-2 focus:outline-primary disabled:cursor-not-allowed disabled:opacity-50',
+            'flex w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus:outline-2 focus:outline-offset-2 focus:outline-primary disabled:cursor-not-allowed disabled:opacity-50 data-[size=sm]:h-8 data-[size=md]:h-9 data-[size=lg]:h-10',
             className,
           )}
           ref={ref}
